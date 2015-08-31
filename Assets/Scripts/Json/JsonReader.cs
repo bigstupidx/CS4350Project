fce795162d4b928ece5080559424b2fa3db3aa34
Shu Hui Yuen <yuenshuhui@gmail.com>
2015-08-31 10:38:34 +0800

    Added json script for testing

---
+++ Assets/Scripts/Json/JsonReader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using LitJson;
+
+public class JsonReader : MonoBehaviour {
+
+	private string jsonString;
+	private JsonData itemData;
+	
+	
+	void Start () {
+		jsonString = File.ReadAllText (Application.dataPath + "/Resources/item.json");
+		itemData = JsonMapper.ToObject (jsonString);
+
+		//Debug.Log (jsonString);
+		Debug.Log (itemData ["item"][3]["name"]);
+		Debug.Log (itemData ["item"][3]["responds_active"][0]);
+	}
+
+}
