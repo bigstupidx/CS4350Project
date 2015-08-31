22efcbd0779156ec7782bb4bc6841220cfa2f665
a0099654 <a0099654@u.nus.edu>
2015-08-31 17:37:31 +0800

    Adding scene for interactable object

---
+++ Assets/addingGUIText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// not working
+public class addingGUIText : MonoBehaviour {
+	public GameObject gui;
+	private Text text;
+	private string textDisplay = null;  //passing from Database (Check again T__T)
+
+	void Awake(){
+		text = gui.GetComponent<Text> ();
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		textDisplay = "Hello World";
+		//text.text = textDisplay.ToString;
+	}
+}
---
+++ Assets/obtainItem.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class obtainItem : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject feedback;
+	public GameObject noObjectFoundFeedback;
+	private int countKeyboardHit = 0;
+	private bool trigger;
+
+	void Start () {
+		feedback.SetActive (false);
+		noObjectFoundFeedback.SetActive (false);
+	}
+
+	void OnTriggerEnter(Collider other){
+		trigger = true;
+	}
+
+	void OnTriggerExit(Collider other){
+		trigger = false;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if(countKeyboardHit == 0 && trigger){
+				feedback.SetActive(true);
+				Destroy (feedback, 5.0f);
+				countKeyboardHit++;
+			}
+			else if(countKeyboardHit !=0 && trigger){
+				noObjectFoundFeedback.SetActive(true);
+			}
+			else{
+				noObjectFoundFeedback.SetActive(false);
+			}
+
+		}
+
+	}
+	
+}
