8a462db8ce7fcbcb78874cf868ed0637a1374b25
Trong Nhan <imouto1994@gmail.com>
2015-09-03 14:47:47 +0800

    [update] Add logic for GameController and PlayerController to load initial data from JSON file

---
+++ Assets/Scripts/Controllers/EndingController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EndingType: int { 
+	Ending1, 
+	Ending2, 
+	Ending3,
+	Ending4,
+	Ending5,
+	EndingCount
+}
+
+public class EndingController : MonoBehaviour {
+
+	private const int ENDING_LIMIT = 100;
+
+	static public EndingController instance;
+
+	int[] endings;
+
+	public void Awake() {
+		instance = this;
+	}
+
+	public void Init() {
+		endings = new int[(int) EndingType.EndingCount];
+	}
+
+	public void ItemTriggered(Item item) {
+		for (int i = 0; i < endings.Length; i++) {
+			if (i < item.endingPoints.Length) {
+				endings[i] += item.endingPoints[i];
+			}
+			if (endings[i] > ENDING_LIMIT) {
+				GameController.instance.GameOver((EndingType) i);
+				break;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Object/Item.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Item : MonoBehaviour {
+
+	public string itemId;
+	public string[] requiredItems;
+	public string[] restrictedItems;
+	public string[] leadItems;
+	public string[] eventDialouge;
+	public string[] defaultDialouge;
+	public int[] endingPoints;
+
+	public void ItemTriggered(Item item) {
+
+	}
+}
