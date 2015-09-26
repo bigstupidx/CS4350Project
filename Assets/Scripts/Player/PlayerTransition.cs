9accc1611ef548d01db8a6d412d0ae331ab7e37e
Shu Hui Yuen <yuenshuhui@gmail.com>
2015-09-26 18:20:20 +0800

    apply stash

--- Assets/Scripts/Player/PlayerTransition.cs
+++ Assets/Scripts/Player/PlayerTransition.cs
@@ -8,13 +8,15 @@ public class PlayerTransition : MonoBehaviour {
 			case "Ground":
 				{
 					Debug.Log("I am going to Ground");
-					Application.LoadLevel("GroundGameScene");
+					//Application.LoadLevel("GroundGameScene");
+					Application.LoadLevel("Testing_scene_1");
 					//LevelHandler.Instance.LoadSpecific ("GroundGameScene");
 					break;
 				}
 			case "Platform":
 				{
-					Application.LoadLevel ("PlatformGameScene");
+					//Application.LoadLevel ("PlatformGameScene");
+					Application.LoadLevel("Testing_scene");
 					break;
 				}
 		}
