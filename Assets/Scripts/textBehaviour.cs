b521907e04b9c7f9a28d51fb452a2d227e8e72ae
renaldy <renaldy_widjaja@yahoo.com>
2015-10-01 11:12:20 +0800

    sound effects and speech

---
+++ Assets/Scripts/textBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class textBehaviour : MonoBehaviour {
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float currTime = Time.time - startTime;
+		Color temp = gameObject.GetComponent<Text> ().color;
+		if (currTime > 1.5f) {
+
+			pos
+			temp.a *= 0.5f;
+			gameObject.GetComponent<Text> ().color = temp;
+			
+		} else if (currTime > 3.0f) {	
+			temp.a = 1.0f;
+			gameObject.GetComponent<Text> ().color = temp;
+			startTime = Time.time;
+		}
+	}
+}
