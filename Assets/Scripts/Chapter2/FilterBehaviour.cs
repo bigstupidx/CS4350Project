24568c743f19c1003c5a815abed6aec8e668c5af
Shu Hui Yuen <yuenshuhui@gmail.com>
2015-10-16 03:30:35 +0800

    Added Placeholder filter for chapter 2

---
+++ Assets/Scripts/Chapter2/FilterBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FilterBehaviour : MonoBehaviour {
+
+	public GameObject filter;
+	// Use this for initialization
+	void Start () {
+		if (EndingController.instance.isChapter2Activated) {
+			filter.SetActive(true);
+		}
+		else
+			filter.SetActive(false);
+	}
+}
