7eb91393ce082fb024715fcd7785d9abf2722cc2
liujialesg <liujialesg@yahoo.com.sg>
2015-10-15 20:55:24 +0800

    Added Skip To Platform from TitleScreen (Press Space) Editted Transition item for Sewage Exit to go to the right place

--- Assets/Scripts/Player/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData.cs
@@ -5,8 +5,8 @@ public class PlayerData : MonoBehaviour {
     public const string PlayerSpriteFilenameStart = "Sprites/PlayerSprites/spr_";
 
 	public static int ParentGenderId = 1; // 1: Male, 2: Female
-    public static int GenderId = 2; // 1: Boy, 2: Girl
-    public static int HairId = 4;
+    public static int GenderId = 1; // 1: Boy, 2: Girl
+    public static int HairId = 1;
 	public static bool MoveFlag = true;
 	public static int nearestObjectId = -1;
 
