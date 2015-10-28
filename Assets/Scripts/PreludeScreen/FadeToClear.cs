c8377724b6818583bbe7139ebb533c776130b49a
Shu Hui Yuen <yuenshuhui@gmail.com>
2015-10-28 21:43:57 +0800

    Added Transition Scene Update UICanvas with new font

---
+++ Assets/Scripts/PreludeScreen/FadeToClear.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeToClear : MonoBehaviour {
+
+	public bool transitionToNextScreen = false;
+	public string targetScene = "";
+
+	private Image myImage;
+
+	public void TransitToNextScene()
+	{
+		Color temp = myImage.color;
+		if (temp.a <= 0.0f) {
+			transitionToNextScreen = true;
+		}
+	}
+	void Start()
+	{
+		myImage = GetComponent<Image> ();
+	}
+	void FixedUpdate () {
+
+		if (transitionToNextScreen) {
+			Color temp = myImage.color;
+			if (temp.a >= 1.0f){
+				if(targetScene.Length > 0)
+					Application.LoadLevel(targetScene);
+				else
+					Debug.Log("ERROR!!! No target scene");
+			}
+			else {
+				temp.a += (0.3f * Time.deltaTime);
+				myImage.color = temp;
+			}
+		} else {
+			Color temp = myImage.color;
+			if( temp.a > 0.0f && temp.a <= 1.0f ){
+				temp.a -= (0.3f * Time.deltaTime);
+				myImage.color = temp;
+			}
+		}
+	
+	}
+}
---
+++ Assets/Scripts/PreludeScreen/TransitionTimerBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TransitionTimerBehaviour : MonoBehaviour {
+
+	private Text timeText;
+	public int startTimeHour;
+	public int startTimeMinutes;
+
+	private float startTime;
+	void Start () {
+		timeText = this.GetComponent<Text> ();
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if ((Time.time - startTime) > 1.0f){
+			startTimeMinutes++;
+			startTime = Time.time;
+		}
+
+		if (startTimeMinutes == 60) {
+			startTimeHour++;
+			startTimeMinutes = 0;
+		}
+
+		string content = startTimeHour + " : " + string.Format("{0:00}", startTimeMinutes) + " pm";
+		timeText.text = content;
+	}
+}
