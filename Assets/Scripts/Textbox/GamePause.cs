261b38fef44f754e07bc8de0ec0b61e8316a4e40
liujialesg <liujialesg@yahoo.com.sg>
2015-11-01 16:51:33 +0800

    [Fix] Return to title bug Polished GamePause

--- Assets/Scripts/Textbox/GamePause.cs
+++ Assets/Scripts/Textbox/GamePause.cs
@@ -39,26 +39,25 @@ public class GamePause: MonoBehaviour {
     }
 
     public void ResumePressed() {
-		isPaused = false;
+
 		GameController.instance.ToggleTimer ();
         pauseButton.SetActive(true);
         pausePanel.SetActive(false);
         textboxScript.enabled = true;
         //blinkingScript.enabled = true;
-        PlayerData.MoveFlag = true;
-
+		isPaused = false;
     }
 
     public void ExitPressed() {
-		isPaused = false;
+
         //blinkingScript.enabled = true;
         textboxScript.enabled = true;
         Destroy(GameController.instance);
         Destroy(PlayerController.instance);
         Destroy(EndingController.instance);
+		Destroy (TraceController.instance);
         LevelHandler.Instance.LoadSpecific("TitleScene");
-        
-        PlayerData.MoveFlag = true;
+		isPaused = false;
     }
 
 }
