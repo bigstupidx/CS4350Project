e8838c2aa3339535765771471cb2ef88423d340a
liujialesg <liujialesg@yahoo.com.sg>
2015-11-01 17:04:44 +0800

    Remove Player Audio Source play on awake

---
+++ Assets/Scripts/Interactable Objects/ObjectSounds.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSounds : MonoBehaviour {
+
+	public AudioClip[] clips;
+	public float MinTimeBeforePlay = 5.0f;
+	public float MaxTimeBeforePlay = 10.0f;
+
+	AudioSource audioSource;
+
+	float timer;
+	// Use this for initialization
+	void Start () {
+		audioSource = transform.GetComponent<AudioSource> ();
+	}
+
+	void ResetTimer (){
+		timer = Random.Range (MinTimeBeforePlay, MaxTimeBeforePlay);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if(timer <= 0.0f && clips.Length > 0){
+
+			audioSource.clip = clips[Random.Range (0,clips.Length)];
+			audioSource.Play ();
+			ResetTimer ();
+		}
+	}
+}
