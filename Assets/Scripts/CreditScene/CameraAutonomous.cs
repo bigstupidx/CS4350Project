e142ff8f4fbe8462e8d861dfe20450f3db8a5546
renaldy <renaldy_widjaja@yahoo.com>
2015-11-08 01:51:26 +0800

    credit Scene is done

--- Assets/Scripts/CreditScene/CameraAutonomous.cs
+++ Assets/Scripts/CreditScene/CameraAutonomous.cs
@@ -1,54 +1,106 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CameraAutonomous : MonoBehaviour {
-	GameObject cameraObject;
-	private float startTime;
-	private float durationTime;
-	private Vector3 cam_movement_z;
-	private Vector3 cam_movement_y;
-	private Vector3 cam_movement_x;
+
+	public GameObject cameraObject;
+	public GameObject background;
+
+	private int currentState;
+	private Color bg_color;
+	private bool pos;
+
+	private float stateTimer;
+	private float threshold;
 	private float _pi;
-	private Vector3 rotateAngle;
-	private Vector3 startPt;
+
+	private Vector3[] cam_movement = new [] { 
+		new Vector3 (1.0f, 0.0f, 0.0f),
+		new Vector3 (0.0f, 1.0f, 0.0f),
+		new Vector3 (0.0f, 0.0f, 1.0f)
+	};
+	private Vector3[] startArr = new [] { 
+		new Vector3 (-14.0f, 20.0f, -11.0f), 
+		new Vector3 (-10.0f, 7.0f, -19.0f), 
+		new Vector3 (-12.4f, 1.9f, -12.5f),
+		new Vector3 (0.0f, 0.0f, 0.0f)
+	};
+	private Vector3[] rotateArr = new [] { 
+		new Vector3 (55.0f, 30.0f, 0.0f),
+		new Vector3 (25.0f, 130.0f, 0.0f),
+		new Vector3 (15.0f, 0.0f, 0.0f),
+		new Vector3 (0.0f, 0.0f, 0.0f)
+	};
 	// Use this for initialization
 	void Start () {
 		_pi = 3.142f;
-		cameraObject = GameObject.FindGameObjectWithTag ("MainCamera");
-		startTime = Time.time;
-		cam_movement_z = new Vector3 (0.0f, 0.0f, 1.0f);
-		cam_movement_x = new Vector3 (1.0f, 0.0f, 0.0f);
-		cam_movement_y = new Vector3 (0.0f, 1.0f, 0.0f);
-
-
+		threshold = 1.0f;
+		currentState = -1;
+		stateTimer = 10.0f;
+		bg_color = background.GetComponent<Image> ().color;
+		pos = true;
 	}
 //	cameraObject.transform.position = startPt;
 //	cameraObject.transform.rotation = Quaternion.Euler(rotate_y);
 	// Update is called once per frame
 	void Update () {
-		durationTime = Time.time - startTime ;
-		if (durationTime > 0.0f && durationTime < 4.0f) {
-			startPt = new Vector3 (-0.508f, -0.045f, 0.349f);
-			rotateAngle = new Vector3 (0.0f, 0.0f, 0.0f);
-			moveCamera (startPt, rotateAngle);
-		} else if (durationTime > 4.0f && durationTime < 20.0f) {
-			cameraObject.transform.Translate (cam_movement_z * -0.03f * Time.deltaTime);
-		} else if (durationTime > 20.0f && durationTime < 21.0f) {
-			rotateAngle = new Vector3 (55.0f, 130.0f, 0.0f);
-			startPt = new Vector3 (0.353f, 0.22f, 0.139f);
-			moveCamera (startPt, rotateAngle);
-		} else if (durationTime > 21.0f && durationTime < 40.0f) {
-			cameraObject.transform.Translate (cam_movement_x * 0.03f * Time.deltaTime);
-		} else if (durationTime > 40.0f && durationTime < 41.0f) {
-			rotateAngle = new Vector3 (55.0f, 220.0f, 0.0f);
-			startPt = new Vector3 (0.521f, 0.147f, -0.069f);
-			moveCamera (startPt, rotateAngle);
-		} else if (durationTime > 42.0f && durationTime < 61.0f) {
-			cameraObject.transform.Translate (cam_movement_x * 0.03f * Time.deltaTime);
+		switch (currentState) {
+		case 0: moveCamera(startArr[0], rotateArr[0]); break;
+		case 1: dragCamera(cam_movement[0], 1.0f); fadeIn (); break;
+		case 2: dragCamera(cam_movement[0], 1.0f);break;
+		case 3: dragCamera(cam_movement[0], 1.0f); fadeOut(); pos = true; break;
+		case 4: moveCamera(startArr[1], rotateArr[1]); dragCamera(cam_movement[0], -1.5f); fadeIn ();  break;
+		case 5: dragCamera(cam_movement[0], -1.5f);break;
+		case 6: dragCamera(cam_movement[0], -1.5f); fadeOut(); pos = true; break;
+		case 7: moveCamera (startArr[2], rotateArr[2]);dragCamera(cam_movement[0], 1.0f); fadeIn ();  break;
+		case 8: dragCamera(cam_movement[0], 1.0f); break;
+		case 9: dragCamera(cam_movement[0], 1.0f); fadeOut(); break;
+		}
+
+		if (stateTimer > 0.0f) {
+			stateTimer -= 10.0f * Time.deltaTime;
+		} else {
+			currentState++;
+			switch (currentState) {
+			case 0: stateTimer = 40.0f;  break;
+			case 1: stateTimer = 10.0f; break;
+			case 2: stateTimer = 155.0f; break;
+			case 3: stateTimer = 10.0f; break;
+			case 4: stateTimer = 10.0f; break;
+			case 5: stateTimer = 155.0f; break;
+			case 6: stateTimer = 10.0f; break;
+			case 7: stateTimer = 10.0f; break;
+			case 8: stateTimer = 155.0f; break;
+			case 9: stateTimer = 10.0f; break;
+			default: stateTimer = 10000.0f; break;
+			}
 		}
 	}
+
 	void moveCamera(Vector3 startPt, Vector3 rotationAngle) {
-		cameraObject.transform.position = startPt;
-		cameraObject.transform.rotation = Quaternion.Euler (rotationAngle);
+		if (pos) {
+			cameraObject.transform.position = startPt;
+			cameraObject.transform.rotation = Quaternion.Euler (rotationAngle);
+		}
+	}
+
+	void dragCamera (Vector3 direction, float speed) {
+		pos = false;
+		cameraObject.transform.Translate (direction * speed * Time.deltaTime);
+	}
+
+	void fadeIn() {
+		if (bg_color.a < 0.50f) {
+			threshold = 0.0f;
+		}
+		bg_color.a -= threshold * Time.deltaTime;
+		threshold = 1.0f;
+		background.GetComponent<Image> ().color = bg_color;
+	}
+
+	void fadeOut() {
+		bg_color.a += 1.0f * Time.deltaTime;
+		background.GetComponent<Image> ().color = bg_color;
 	}
 }
--- Assets/Scripts/CreditScene/TextScroll_Up.cs
+++ Assets/Scripts/CreditScene/TextScroll_Up.cs
@@ -3,51 +3,70 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class TextScroll_Up : MonoBehaviour {
-
-	private float startTime;
-	private float duration;
 	public GameObject logo;
 	public GameObject header;
 	public GameObject text;
+	
 	private Color logo_temp;
 	private Color header_temp;
 	private Color text_temp;
 
-
+	private float stateTimer;
+	private int currentState;
 
 	// Use this for initialization
 	void Start () {
 		text_temp = text.GetComponent<Image> ().color;
 		logo_temp = logo.GetComponent<Image>().color;
 		header_temp = header.GetComponent<Image> ().color;
+
 		header_temp.a = 0.0f;
 		logo_temp.a = 0.0f;
 		text_temp.a = 0.0f;
+
 		text.GetComponent<Image> ().color = text_temp;
 		header.GetComponent<Image> ().color = header_temp;
 		logo.GetComponent<Image> ().color = logo_temp;
-		startTime = Time.time;
+
+		currentState = 0;
+		stateTimer = 20.0f;
 	}
-	
+
+	void run()
+	{
+		switch (currentState) {
+		case 0: logo_temp.a += 1.0f * Time.deltaTime; logo.GetComponent<Image>().color = logo_temp; break;
+		case 1: header_temp.a += 1.0f * Time.deltaTime; header.GetComponent<Image> ().color = header_temp; break;
+		case 2: text_temp.a += 1.0f * Time.deltaTime; text.GetComponent<Image> ().color = text_temp; break;
+		case 3: up(); break;
+		case 4: text_temp.a -= 1.0f * Time.deltaTime; text.GetComponent<Image> ().color = text_temp; break;
+		case 5: Application.LoadLevel("TitleScene"); break;
+		}
+		if (stateTimer > 0.0f) {
+				stateTimer -= 10 * Time.deltaTime;
+		} else {
+			currentState++;
+			switch (currentState) {
+			case 0: stateTimer = 20.0f; break;
+			case 1: stateTimer = 20.0f; break;
+			case 2: stateTimer = 20.0f; break;
+			case 3: stateTimer = 590.0f; break;
+			case 4: stateTimer = 60.0f; break;
+			case 5: stateTimer = 10.0f; break;
+			default: stateTimer = 100000.0f; break;
+			}
+		}
+		Debug.Log (currentState + " " + stateTimer);
+	}
+
+	void up() {
+		text.transform.Translate (Vector3.up * 30.0f * Time.deltaTime);
+		logo.transform.Translate (Vector3.up * 30.0f * Time.deltaTime);
+		header.transform.Translate (Vector3.up * 30.0f * Time.deltaTime);
+	}
+
 	// Update is called once per frame
 	void Update () {
-		duration = Time.time - startTime;
-		if (duration > 3.5f && duration < 6.0f) {
-			logo_temp.a += 1.0f * Time.deltaTime;
-			logo.GetComponent<Image> ().color = logo_temp;
-		} else if (duration > 6.0f && duration < 8.0f) {
-			header_temp.a += 1.0f * Time.deltaTime;
-			header.GetComponent<Image> ().color = header_temp;
-		} else if (duration > 8.0f && duration < 12.0f) {
-			text_temp.a += 1.0f * Time.deltaTime;
-			text.GetComponent<Image> ().color = text_temp;
-		} else if (duration > 12.0f && duration < 76.0f) {
-			transform.Translate (Vector3.up * 30.0f * Time.deltaTime);
-		} else if (duration > 78.0f && duration < 80.0f) {
-			text_temp.a -= 1.0f * Time.deltaTime;
-			text.GetComponent<Image> ().color = text_temp;
-		} else if (duration > 82.0f) {
-			Application.LoadLevel("TitleScene");
-		}
+		run ();
 	}
 }
